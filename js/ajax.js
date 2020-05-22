567de5a105c64d97850e5f179edb340381cb8590
g123456 <gaokangni@cangbaopai.com>
2020-05-22 17:46:58 +0800

    前端集合

--- js/ajax.js
+++ js/ajax.js
@@ -1,12 +1,19 @@
 $.ajax({ 
-	    type: "get",
-	    url:'https://gknclound.github.io/co/g.json',
-	    cache: false,
-	    dataType: "json",
-	    jsonp: "callback",
-	    complete:function(a){
-	    	var strJSON =a.responseText
-			var obj = new Function("return" + strJSON)();//转换后的JSON对象
-			console.log(obj);
-        },
+    type: "get",
+    url:'https://gknclound.github.io/co/g.json',
+    cache: false,
+    dataType: "json",
+    jsonp: "callback",
+    complete:function(a){
+    	var strJSON =a.responseText
+		var obj = new Function("return" + strJSON)();//转换后的JSON对象
+		console.log(obj);
+		var html=obj.html
+		for(var i=0;i<html.length;i++){
+			let t=html[i].name
+//			let s=html[i].img
+			let o='<li class="mui-table-view-cell mui-media"><a class="mui-navigate-right"><img class="mui-media-object mui-pull-left" src=""><div class="mui-media-body">CBD<p class="mui-ellipsis">'+t+'</p></div></a></li>'
+			$('#tabbar1').append(o)
+		}
+    },
 })	
