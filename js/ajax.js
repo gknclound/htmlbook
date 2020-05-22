8a3562f3a3c17a3a8d804002b3fec7ea5ed89574
g123456 <gaokangni@cangbaopai.com>
2020-05-22 17:18:29 +0800

    前端集合

---
+++ js/ajax.js
@@ -0,0 +1,14 @@
+function(){
+	$.ajax({ 
+	    type: "get",
+	    url:'https://gknclound.github.io/co/g.json',
+	    cache: false,
+	    dataType: "json",
+	    jsonp: "callback",
+	    complete:function(a){
+	    	var strJSON =a.responseText
+			var obj = new Function("return" + strJSON)();//转换后的JSON对象
+			console.log(obj);
+        },
+	})	
+}
